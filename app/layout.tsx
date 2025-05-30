5cd629085c3d03c5a1c437201ec1f8a9f1f7ac2e
Gersi Uta <75692920+Gersi266@users.noreply.github.com>
2025-05-30 12:52:41 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
+      <head></head>
       <body className={inter.className}>
         <LanguageProvider>
           <ContentProvider>
